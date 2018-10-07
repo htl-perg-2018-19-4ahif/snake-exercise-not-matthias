8604ed5e403b0b316916d995a7c0ca9adffb4dd1
Matthias <26800596+not-matthias@users.noreply.github.com>
2018-10-07 20:43:01 +0200

    Removed chalk-animation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 // Clear the console to hide the execute command
 process.stdout.write('\x1Bc');
 
-// Cool logo
-// const chalkAnimation = require('chalk-animation');
-// const renderer = require('./renderer')
-// chalkAnimation.rainbow('\n\n\t\t\t\t\t\t\t\tSnake by not-matthias');
-
 
 // ###########################################
 //              Input listener
